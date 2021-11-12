693b9f5487624e8f91b1ea80f34691eb51a2a1a3
Irina-S <serysheva.i@yandex.ru>
2021-11-13 03:58:28 +0900

    Готово

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row mt-5 mb-4">
+        <div class="col-12">
+          <b-form-select v-model="selectedUser" :options="usersWithDefault" :disabled="isUserSelectDisabled"></b-form-select>
+        </div>
+      </div>
+      <div class="row">
+        <Post
+          v-for="post in postsFilteredByUser"
+          :key="post.id"
+          :id="post.id"
+          :userId="post.userId"
+          :userName="getUserNameById(post.userId)"
+          :title="post.title"
+          :body="post.body"
+        ></Post>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import Post from "./components/Post.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Post
   },
+
+  data(){
+    return{
+        apiBaseUrl:'http://jsonplaceholder.typicode.com/',
+        usersApi:'users',
+        postsApi:'posts',
+        selectedUser: null,
+        users: [],
+        posts:[]
+    }
+  },
+
+  computed:{
+    /**
+     * Список пользователей, дополненный нулевым значением
+     */
+    usersWithDefault(){
+      return [
+        { value: null, text: 'All'},
+        ...this.users
+      ]
+    },
+
+    /**
+     * Определяет кликабельность списка пользователей, в зависимости от того подгружен ли список
+     */
+    isUserSelectDisabled(){
+      return this.users.length>0?false:true;
+    },
+
+    /**
+     * Отфильтрованные посты по автору
+     */
+    postsFilteredByUser(){
+      if (this.selectedUser===null){
+        return this.posts
+      }
+      else{
+        return this.posts.filter((post)=>{
+          return post.userId===this.selectedUser;
+        })
+      }
+    }
+  },
+
+  methods:{
+
+    /**
+     * Отправление запроса на получение списка пользователей
+     */
+    async getUsers(){
+      try{
+        const response = await axios.get(`${this.apiBaseUrl}${this.usersApi}`);
+        const users = response.data;
+        this.users = users.map((user)=>{
+          return {
+            value:user.id,
+            text:user.name
+          }
+        });
+      } catch (error){
+        console.log(error);
+      }
+    },
+
+    /**
+     * Отправление запроса на получение списка постов
+     */
+    async getPosts(){
+      try{
+        const response = await axios.get(`${this.apiBaseUrl}${this.postsApi}`);
+        const posts = response.data;
+        console.log(posts);
+        this.posts = posts;
+      } catch (error){
+        console.log(error);
+      }
+    },
+
+    getUserNameById(id){
+      console.log(id);
+      return this.users.filter((user)=>{
+        return user.value==id;
+      })
+      .pop().text;
+    }
+
+  },
+
+  mounted(){
+    this.getUsers();
+    this.getPosts();
+  }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'node_modules/bootstrap/scss/bootstrap.scss';
+  @import "~@/assets/scss/vendors/bootstrap-vue/index";
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="col-md-6 col-lg-4 mb-4">
+        <b-card 
+            class="post" 
+            :title="title" 
+            :sub-title="userName"
+        >
+            <b-card-text>
+                {{body}}
+            </b-card-text>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Post",
+    props:{
+        id:{
+            type:Number,
+            required:true
+        },
+        userId:{
+            type:Number,
+            required:true
+        },
+        userName:{
+            type:String,
+            required:true
+        },        
+        title:{
+            type:String,
+            required:true
+        },
+        body:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
+
+<style>
+    .post{
+        height: 100%;
+    }
+</style>
